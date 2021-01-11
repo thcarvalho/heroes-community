955575498324270c0c0aaa7126bac57ae6488b02
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2021-01-10 22:45:03 -0300

    updated migrations

---
+++ api/Migrations/20210110024843_LikeMigration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace api.Migrations
+{
+    public partial class LikeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Heroes",
+                type: "character varying(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Heroes",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Like",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Like", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Like_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_UserId",
+                table: "Like",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Like");
+
+            migrationBuilder.AddColumn<int[]>(
+                name: "Likes",
+                table: "Users",
+                type: "integer[]",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Heroes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Heroes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+        }
+    }
+}
---
+++ api/Migrations/20210110031948_LikeFK.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class LikeFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Like_Users_UserId",
+                table: "Like");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Like",
+                table: "Like");
+
+            migrationBuilder.RenameTable(
+                name: "Like",
+                newName: "Likes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Like_UserId",
+                table: "Likes",
+                newName: "IX_Likes_UserId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LikedUser",
+                table: "Likes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Likes",
+                table: "Likes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Users_UserId",
+                table: "Likes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Likes",
+                table: "Likes");
+
+            migrationBuilder.DropColumn(
+                name: "LikedUser",
+                table: "Likes");
+
+            migrationBuilder.RenameTable(
+                name: "Likes",
+                newName: "Like");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Likes_UserId",
+                table: "Like",
+                newName: "IX_Like_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Like",
+                table: "Like",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Like_Users_UserId",
+                table: "Like",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
