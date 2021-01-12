03e9249e9ba010fd82ab177e1008cfc3a9be5423
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2021-01-12 13:08:42 -0300

    add userheroes to data context

--- api/Data/DataContext.cs
+++ api/Data/DataContext.cs
@@ -6,11 +6,12 @@ namespace api.Data
   public class DataContext : DbContext
   {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
-    {}
+    { }
 
     public DbSet<Hero> Heroes { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Like> Likes { get; set; }
     public DbSet<Image> Images { get; set; }
+    public DbSet<UserHeroes> UserHeroes { get; set; }
   }
 }
