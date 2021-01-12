d1ef66f1984e869108bb1948fbaa8b6d0496a559
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2021-01-11 23:40:28 -0300

    hero migration updated

---
+++ api/Migrations/20210112000820_HeroUpdated.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class HeroUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Image_Heroes_HeroId",
+                table: "Image");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Image",
+                table: "Image");
+
+            migrationBuilder.RenameTable(
+                name: "Image",
+                newName: "Images");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Image_HeroId",
+                table: "Images",
+                newName: "IX_Images_HeroId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Heroes",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OriginalName",
+                table: "Heroes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HeroId",
+                table: "Images",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Images",
+                table: "Images",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Heroes_HeroId",
+                table: "Images",
+                column: "HeroId",
+                principalTable: "Heroes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Heroes_HeroId",
+                table: "Images");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Images",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "OriginalName",
+                table: "Heroes");
+
+            migrationBuilder.RenameTable(
+                name: "Images",
+                newName: "Image");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Images_HeroId",
+                table: "Image",
+                newName: "IX_Image_HeroId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "Heroes",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "HeroId",
+                table: "Image",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Image",
+                table: "Image",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Image_Heroes_HeroId",
+                table: "Image",
+                column: "HeroId",
+                principalTable: "Heroes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
