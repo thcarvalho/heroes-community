134082d2153c47e159a75c62b6c2ed552a763200
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2021-01-11 23:40:05 -0300

    translate hero bio to portuguese

--- api/Controllers/HeroController.cs
+++ api/Controllers/HeroController.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using api.Data;
 using api.Models;
 using api.Repositories.Heroes;
+using api.Repositories.Images;
+using api.Services.MarvelAPI;
+using api.Services.TranslationAPI;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -35,14 +39,38 @@ namespace api.Controllers
     [HttpPost("")]
     public async Task<ActionResult<Hero>> Post(
       [FromServices] IHeroRepository heroRepository,
+      [FromServices] IImageRepository imageRepository,
       [FromServices] DataContext context,
+      [FromServices] IMarvelAPIService marvelAPI,
+      [FromServices] ITranslationService translationService,
       [FromBody] Hero hero
     )
     {
       if (ModelState.IsValid)
       {
-        await heroRepository.Save(hero);
-        return hero;
+        try
+        {
+          var response = await marvelAPI.GetData(hero.OriginalName);
+
+          hero.Bio = translationService.TranslateText(response.data.results[0].description).ToString();
+
+          var heroImg = response.data.results[0].thumbnail;
+          Image img = new Image();
+
+          ActionResult<Hero> res = await heroRepository.Save(hero);
+          
+          img.HeroId = res.Value.Id;
+          img.Url = $"{heroImg.path}.{heroImg.extension}";
+
+          await imageRepository.Save(img);
+
+          return hero;
+        }
+        catch (ArgumentOutOfRangeException exception)
+        {
+          throw exception;
+        }
+
       }
       return BadRequest(ModelState);
     }
